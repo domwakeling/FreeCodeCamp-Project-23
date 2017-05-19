180e601b317a4539f900f2a67108d715a1b6348f
domwakeling <domwakeling@gmail.com>
2017-05-19 23:00:15 +0000

    Add view and start re-writing server.js
    Add views folder (one view and scss/css styles) to serve root page with
    instructions. Timestamp microserver used as base, note that the URLs for
    Heroku still need to be updated once ready to push.
    
    Add key for mongoURL (using a Sandbox at MLab), add environment check for
    value of key (local version stored in keys.js in a module.export object, which
    is not stored in this git).

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
 var express = require('express');
+var path = require('path');
+var sassMiddleware = require('node-sass-middleware');
+// var url = require('url');
 var portToUse = process.env.PORT || 8080;
+var mongoURL = process.env.MONGO_URL || require('./keys.js').mongoURL;
+
+var months = ['January', 'February', 'March', 'April',
+              'May', 'June', 'July', 'August', 'September',
+              'October', 'November', 'December'];
 
 var app = express();
 
-app.all('*', function(req,res) {
+function naturalDate(date) {
+    return months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+}
+
+app.use(sassMiddleware(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('*', function(req,res) {
     
-    var header = req.headers;
-    var url = header['x-forwarded-for'];
-    var lang = header['accept-language'].split(',')[0];
-    var sys = header['user-agent'];
-    sys = sys.substring(sys.indexOf("(") + 1, sys.indexOf(")"));
-    
-    var ret = {
-        'ipaddress' : url,
-        'language' : lang,
-        'software' : sys
+    if(req.url.length > 1) {
+        // set up an empty return object "in case"
+        var ret = {
+            'unix': null,
+            'natural' : null
+        };
+        // get the potential date info
+        var potDate = req.url.substr(1).replace(/%20/g, " ");
+        // check whether a number, in which case timestamp
+        if (!/\D/.test(potDate)) {
+            var myDate = new Date(parseInt(potDate) * 1000)
+            ret.unix = parseInt(potDate);
+            ret.natural = naturalDate(myDate);
+        }
+        // create a date; may be invalid ...
+        var myDate = new Date(potDate);
+        // if valid, do thing
+        if(!isNaN(myDate.getHours())) {
+            ret.natural = naturalDate(myDate);
+            ret.unix = Math.floor(myDate.getTime() / 1000);
+        }
+        res.json(ret);
     }
-    
-    res.json(ret);
 });
 
 app.listen(portToUse, function() {
